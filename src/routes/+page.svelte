92ee9b6af078e55fab0e6b1fd5fc0759fdb54e64
Surajit Sutradhar <surajit_ug_23@ee.nits.ac.in>
2024-12-21 12:37:48 +0530

    Second update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<div id="overlay"> 
+<div id="overlay" class="fixed-bottom-right"> 
 <!-- navbar -->
 </div>
 <section class="min-h-screen flex flex-col">
@@ -9,23 +9,23 @@
         <p class="text-[0.833vw] font-sen text-[#CABBFF] select-none ">DESIGNER <span class="font-charm lg:text-[0.9vw]">&</span> DEVELOPER</p>
     </div>
     <div class="mx-[32px] my-[32px]">
-        <p class="text-[32px] leading-[82%] tracking-tighter text-[#D9D9D9] font-sen">
-            “Simplicity will stand out, while complexity <br/> will get lost in the crowd.”<br/>
+        <p class="text-[32px] leading-[82%] tracking-tighter text-[#D9D9D9] font-sen custom-selection">
+            “Simplicity will stand out, while complexity <br class="custom-selection"/>will get lost in the crowd.”<br/>
                                           ~Kevin Barnett
         </p>
     </div>
 </section>
 
 <section id="about" class="min-h-screen flex">
-    <div class="mr-[25.104vw] w-full flex flex-row justify-between relative">
+    <div class="pr-[25.104vw] w-full flex flex-row justify-between relative">
         <div class="w-[33.125vw] flex flex-col justify-end">
             <div class="pl-[32px] pb-[3.333vw]">
-                <p class="font-sen font-medium leading-[100%] text-[1.667vw] text-white">"Passionate about creating visually captivating and user-centric designs, I am a designer who thrives on turning ideas into stunning digital experiences. ut the way people interact with the world."</p>
+                <p class="font-sen font-medium leading-[100%] text-[1.667vw] text-white custom-selection">"Passionate about creating visually captivating and user-centric designs, I am a designer who thrives on turning ideas into stunning digital experiences. ut the way people interact with the world."</p>
                 <h1 class="font-sen font-bold leading-[100%] text-white text-[9.375vw] pt-[3.333vw] select-none">about</h1>
             </div>
         </div>
         <div class="w-[32.292vw] py-[3.33vw] flex flex-col justify-between">
-            <p class="font-medium text-[1.042vw] leading-[100%] text-white text-right">
+            <p class="font-medium text-[1.042vw] leading-[100%] text-white text-right custom-selection">
                 "Passionate about creating visually captivating and user-centric designs, I am a designer who thrives on turning ideas into stunning digital experiences. With a keen eye for detail and a love for innovation, I specialize in crafting designs that not only look beautiful but also deliver seamless functionality. Whether it’s UI/UX, graphic design, or creative branding, I aim to tell compelling stories through visuals that leave a lasting impression. Constantly exploring trends and pushing boundaries, I believe in the power of design to transform the way people interact with the world."
             </p>
             <div class="w-full h-[32.292vw] bg-gray-400">
@@ -35,41 +35,84 @@
     </div>
 </section>
 
-<section id="about-ctd" class="min-h-screen flex">
-    <div class="w-[49.844vw] mx-auto gap-0 relative">
+<section id="about-ctd" class="min-h-screen flex flex-col">
+    <div class="w-[49.844vw] mx-auto my-[3.33vw] gap-0">
+        <!-- Section 1 -->
         <div class="h-[15.104vw] relative">
-            <div class="h-full w-[13.75vw] bg-green-600">
-                
+            <div class="w-[13.75vw] flex flex-col">
+                <div class="w-full h-[12vw] bg-green-600">
+                    <!-- Insert image here -->
+                </div>
+                <span class="font-sen h-[1.67vw] text-[#D9D9D9] custom-selection">
+                    My Github
+                </span>
             </div>
         </div>
+        
+        <!-- Section 2 -->
         <div class="h-[18.49vw] relative">
-            <div class="h-full w-[15.625vw] bg-blue-400 absolute right-0">
-
+            <div class="w-[15.625vw] flex flex-col absolute right-0">
+                <div class="w-full h-[16vw] bg-blue-400">
+                    <!-- Insert image here -->
+                </div>
+                <span class="font-sen h-[1.67vw] text-[#D9D9D9] custom-selection">
+                    My Reads
+                </span>
             </div>
         </div>
-        <div class="h-[15.313vw] relative">
-            <div class="h-full w-[19.792vw] bg-yellow-300">
 
+        <!-- Section 3 -->
+        <div class="h-[15.313vw] relative">
+            <div class="w-[19.792vw] flex flex-col">
+                <div class="w-full h-[13.5vw] bg-yellow-300">
+                    <!-- Insert image here -->
+                </div>
+                <span class="font-sen h-[1.67vw] text-[#D9D9D9] custom-selection">
+                    My Blogs
+                </span>
             </div>
         </div>
-        <div class="h-[15.313vw] relative">
-            <div class="h-full w-[15.625vw] bg-red-700 absolute right-0">
 
+        <!-- Section 4 -->
+        <div class="h-[15.313vw] relative">
+            <div class="w-[15.625vw] flex flex-col absolute right-0">
+                <div class="w-full h-[13.5vw] bg-red-700">
+                    <!-- Insert image here -->
+                </div>
+                <span class="font-sen h-[1.67vw] text-[#D9D9D9] custom-selection">
+                    My Spotify
+                </span>
+            </div>
+        </div>
+    </div>
+
+    <!-- works -->
+    <div id="works" class="w-[49.844vw] h-[16.39vw] mx-auto my-[3.33vw]">
+        <div class="pt-[3.33vw] px-[3.33vw] border-b-[1.5px] border-[#FFFFFF]">
+            <h2 class="font-sen font-bold text-[1.667vw] text-[#D9D9D9] select-none">Stuff I made</h2>
+        </div>
+        <div id="project-info" class="py-[1.667vw] px-[3.333vw] h-[6.39vw] border-b-[1.5px] border-[#FFFFFF] flex flex-row items-center justify-between">
+            <h3 class="font-sen font-regular text-[2.083vw] text-white select-none">This Portfolio</h3>
+            <div>
+                <img src="/linklogo.svg" alt="External Link Svg" class="w-[4.427vw] h-[3.056vw]  hover:shadow-[0_0_10px_#D9D9D9] rounded-full transition-shadow duration-600 ease-in-out cursor-pointer">
             </div>
         </div>
     </div>
 </section>
 
-<section id="contact" class="min-h-screen flex">
-
+<section id="contact" class="min-h-screen pr-[25.104vw] w-full flex flex-row justify-between relative">
+    <div class="w-[33.125vw] flex flex-col justify-end">
+        <div class="pl-[32px] pb-[3.333vw]">
+            <p class="font-sen font-medium leading-[100%] text-[1.667vw] text-white custom-selection"><a href="https://www.linkedin.com/in/surajit-sutradhar?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank">LinkedIn</a></p>
+            <p class="font-sen font-medium leading-[100%] text-[1.667vw] text-white custom-selection"><a href="https://www.behance.net/surajitsutradh2" target="_blank">Behance</a></p>
+            <p class="font-sen font-medium leading-[100%] text-[1.667vw] text-white custom-selection"><a href="https://www.facebook.com/share/Rn8gVZ7AkDwHSH47/" target="_blank">Facebook</a></p>
+            <p class="font-sen font-medium leading-[100%] text-[1.667vw] text-white custom-selection"><a href="https://pin.it/VaLaQDlT6" target="_blank">Pinterest</a></p>
+            <p class="font-sen font-medium leading-[100%] text-[1.667vw] text-white custom-selection"><a href="https://x.com/_the_crowww?t=-PVfGMsz4Gq0Sxu6Y1b1mA&s=09" alt="Twitter Handle Link" target="_blank">X</a></p>
+            <h1 class="font-sen font-bold leading-[100%] text-white text-[9.375vw] pt-[3.333vw] select-none">let's chat</h1>
+        </div>
+    </div>
 </section>
   
-
-
-
-
-
-
 
 
 
@@ -106,16 +149,27 @@
     background-attachment: scroll; /* Fallback for mobile devices */
 }
 
+:global(.custom-selection::selection) {
+    background-color: #D9D9D9; /* Selection background color */
+    color: #73099D; /* Text color during selection */
+}
+
+.fixed-bottom-right {
+    position: fixed;
+    bottom: 20px; /* Distance from the bottom */
+    right: 20px;  /* Distance from the right */
+    width: 50px;  /* Width of the div */
+    height: 50px; /* Height of the div */
+    background-color: red; /* Background color */
+    border-radius: 50%; /* Optional: Make it circular */
+    box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.2); /* Optional: Adds shadow for a more prominent look */
+    z-index: 200; /* Ensure it stays above other elements */
+}
+
 @media (min-width: 768px) {
     :global(body) {
         background-attachment: fixed;
     }
-}
-
-/* Ensure that the flex container takes up the full height */
-:global(.flex) {
-    display: flex;
-    height: 100vh; /* Take up the full height of the viewport */
 }
 
 </style>
