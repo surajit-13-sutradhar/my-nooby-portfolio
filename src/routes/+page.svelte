30a8c38c86d99b42a6372738cf512dfb7ebc478c
Surajit Sutradhar <surajit_ug_23@ee.nits.ac.in>
2024-12-21 09:30:49 +0530

    First Update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,68 @@
-<div class="flex flex-col gap-0 items-center justify-center h-full">
-    <h1 class="text-[2.578vw] text-white font-charm leading-[97%] select-none">
-        Surajit <br/> Sutradhar
-    </h1>
-    <p class="text-[0.833vw] font-sen text-[#CABBFF] select-none ">DESIGNER <span class="font-charm lg:text-[0.9vw]">&</span> DEVELOPER</p>
+<div id="overlay"> 
+<!-- navbar -->
 </div>
+<section class="min-h-screen flex flex-col">
+    <div class="flex flex-col gap-0 items-center justify-center flex-grow">
+        <h1 class="text-[2.578vw] text-white font-charm leading-[97%] select-none">
+            Surajit <br/> Sutradhar
+        </h1>
+        <p class="text-[0.833vw] font-sen text-[#CABBFF] select-none ">DESIGNER <span class="font-charm lg:text-[0.9vw]">&</span> DEVELOPER</p>
+    </div>
+    <div class="mx-[32px] my-[32px]">
+        <p class="text-[32px] leading-[82%] tracking-tighter text-[#D9D9D9] font-sen">
+            “Simplicity will stand out, while complexity <br/> will get lost in the crowd.”<br/>
+                                          ~Kevin Barnett
+        </p>
+    </div>
+</section>
+
+<section id="about" class="min-h-screen flex">
+    <div class="mr-[25.104vw] w-full flex flex-row justify-between relative">
+        <div class="w-[33.125vw] flex flex-col justify-end">
+            <div class="pl-[32px] pb-[3.333vw]">
+                <p class="font-sen font-medium leading-[100%] text-[1.667vw] text-white">"Passionate about creating visually captivating and user-centric designs, I am a designer who thrives on turning ideas into stunning digital experiences. ut the way people interact with the world."</p>
+                <h1 class="font-sen font-bold leading-[100%] text-white text-[9.375vw] pt-[3.333vw] select-none">about</h1>
+            </div>
+        </div>
+        <div class="w-[32.292vw] py-[3.33vw] flex flex-col justify-between">
+            <p class="font-medium text-[1.042vw] leading-[100%] text-white text-right">
+                "Passionate about creating visually captivating and user-centric designs, I am a designer who thrives on turning ideas into stunning digital experiences. With a keen eye for detail and a love for innovation, I specialize in crafting designs that not only look beautiful but also deliver seamless functionality. Whether it’s UI/UX, graphic design, or creative branding, I aim to tell compelling stories through visuals that leave a lasting impression. Constantly exploring trends and pushing boundaries, I believe in the power of design to transform the way people interact with the world."
+            </p>
+            <div class="w-full h-[32.292vw] bg-gray-400">
+                
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="about-ctd" class="min-h-screen flex">
+    <div class="w-[49.844vw] mx-auto gap-0 relative">
+        <div class="h-[15.104vw] relative">
+            <div class="h-full w-[13.75vw] bg-green-600">
+                
+            </div>
+        </div>
+        <div class="h-[18.49vw] relative">
+            <div class="h-full w-[15.625vw] bg-blue-400 absolute right-0">
+
+            </div>
+        </div>
+        <div class="h-[15.313vw] relative">
+            <div class="h-full w-[19.792vw] bg-yellow-300">
+
+            </div>
+        </div>
+        <div class="h-[15.313vw] relative">
+            <div class="h-full w-[15.625vw] bg-red-700 absolute right-0">
+
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="contact" class="min-h-screen flex">
+
+</section>
   
 
 
@@ -39,8 +98,7 @@
 }
 :global(main) {
     position: relative; /* Ensure the content scrolls over the background */
-    z-index: 1;
-    padding: 0 1.7vw; /* Add padding to avoid content touching the edges */
+    z-index: 1; /* Add padding to avoid content touching the edges */
     color: #333; /* Text color for readability */
 }
 
