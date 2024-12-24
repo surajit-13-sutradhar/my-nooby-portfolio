25fd860ed251d8d7239dd5a9c2b7ee52122dd7fc
Surajit Sutradhar <surajit_ug_23@ee.nits.ac.in>
2024-12-24 17:46:00 +0530

    Updated

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -56,6 +56,7 @@
         if (!name.trim() || !email.trim() || !message.trim()) {
             // Show the warning alert here
             showWarning = true
+            alert("Please fillup all the fields before sending")
             resetAlert()
             return; // Stop execution if validation fails
         }
@@ -79,11 +80,13 @@
             console.log(response)
             // Show the success alert here
             showSuccess = true
+            alert("Your message was sent! I will contact you soon:)")
             resetAlert()
             
         } else {
             // Show the error alert here
             showError = true
+            alert("Failed to send message :( Check your connectivity or try again later")
             resetAlert()
         }
 
@@ -93,12 +96,22 @@
         message = ''
     }
 
-    // Navbar hide on scroll
-    let y = $state(0)
+    // Navbar Toggle
+    let isNavOpen = $state(false)
+    let isToggling = $state(false)
+
+    // toggle function
+    function toggleNav() {
+        // prevent rapid toggling while the ani
+        if(isToggling) return
+        isNavOpen = !isNavOpen
+    }
+
+    
+    
 
 </script>
 
-<svelte:window bind:scrollY="{y}" />
 
 <!-- ---------- Success Alert Card ----------- -->
 <div class="z-99999 left-1/2 fixed z-50 hidden">
@@ -118,41 +131,50 @@
 
 
 <!-- ---------- DATE AND TIME ---------- -->
-<div id="date-time" class="fixed-bottom-right m-[1.67vw] tracking-custom58 font-sen text-[16px] font-regular text-[#FFFFFF] pr-[1.67vw] custom-selection "> 
+<div id="date-time" class="fixed-top fixed-bottom-right m-[1.67vw] tracking-custom58 font-sen text-[16px] font-regular text-[#FFFFFF] pr-[1.67vw] custom-selection "> 
     {date.getHours()}:{pad(date.getMinutes())}:{pad(date.getSeconds())}
 </div>
 
 <!-- ---------- NAVBAR ---------- -->
-<div id="navbar" class="top-0 left-1/2 transform -translate-x-1/2 min-w-[90lvw] px-[1.5vw] py-[0.835vw] mx-auto my-[0.835vw] fixed z-40 flex justify-between items-center backdrop-blur-2xl rounded-full border-[0.5px] border-[#c9c9c9]">
-    <!-- Assam India SVG -->
-    <div>
-        <img src="/assamIndia.svg" alt="Assam India" class="w-[5.365vw] h-[2.917vw] select-none" draggable="false" style="pointer-events: none; -webkit-user-drag: none;">
-    </div>
 
-    <!-- Navlinks -->
-    <div id="navLinks" class="flex font-sen font-regular">
-        <a href="#home" class="hover-underline-animation  font-thin center m-[16px] text-[#FFFFFF] custom-selection"
-            >Home</a>
-        <a href="#about" class="hover-underline-animation  font-regular center m-[16px] text-[#FFFFFF] custom-selection" >About</a>
-        <a href="#contact" class="font-regular px-[16px] py-[4px] bg-[#3872f0] text-[#FFFFFF] rounded-full custom-selection flex justify-center items-center border-[1px] border-transparent hover:bg-[#2a2a5f] hover:text-[#FFFFFF] hover:border-[#3872f0] transition duration-300 ease-in-out">Contact</a>
-    </div>
+<!-- Assam India SVG -->
+<div class="assam-svg left-1 top-1 fixed z-30">
+    <img src="/assamIndia.svg" alt="Assam India" class="w-[5.365vw] h-[2.917vw] select-none " draggable="false" style="pointer-events: none; -webkit-user-drag: none;">
 </div>
+
+<!-- Toggle Button -->
+<button 
+    class="mobile-nav-toggle absolute" aria-controls="primary-navigation" aria-expanded={isNavOpen}
+    class:close={isNavOpen}
+    class:hamburger={!isNavOpen}
+    onclick={toggleNav}
+>
+    <span class="sr-only">{isNavOpen ? 'CLose Menu' : 'Open Menu'}</span>
+</button>
+
+<!-- Navlinks or Primary navigation -->
+<header id="navLinks" class="primary-navigation flex font-sen font-regular fixed z-30 right-[1.67vw] top-1" class:open={isNavOpen}>
+    <a href="#home" class="hover-underline-animation  font-thin text-[14px] center m-[16px] text-[#FFFFFF] custom-selection"
+        ><span aria-hidden="true">Home</span></a>
+    <a href="#about" class="hover-underline-animation  font-regular text-[14px] center m-[16px] text-[#FFFFFF] custom-selection" >About</a>
+    <a href="#contact" class="hover-underline-animation  font-bold text-[14px] center m-[16px] text-[#93e0ff] custom-selection" ><span aria-hidden="true">Contact</span></a>
+</header>
 
 <!-- --------------- HOME SECTION ----------------  -->
 <section id="home" class="min-h-screen flex flex-col relative">
     
     <div class="flex flex-col gap-0 items-center justify-center flex-grow">
-        <div transition:fly={{y:50, duration: 2000, delay: 500, easing: t => t * t}}>
-            <h1 class="text-[2.578vw] text-white font-charm leading-[97%] select-none">
+        <div class="intro">
+            <h1 class="my-name text-[2.578vw] text-white font-charm leading-[97%] select-none">
                 Surajit <br/> Sutradhar
             </h1>
-            <p class="text-[0.833vw] font-sen text-[#CABBFF] select-none">DESIGNER <span class="font-charm lg:text-[0.9vw]">&</span> DEVELOPER</p>
+            <p class="my-designnation text-[0.833vw] font-sen text-[#CABBFF] select-none">DESIGNER <span class="font-charm lg:text-[0.9vw]">&</span> DEVELOPER</p>
         </div>
         
-        <div class="mx-[32px] my-[32px]  absolute left-0 bottom-0">
-            <p class="text-[1.67vw] leading-[82%] tracking-tighter text-[#D9D9D9] font-sen custom-selection">
-                “Simplicity will stand out, while complexity <br class="custom-selection"/>will get lost in the crowd.”<br class="custom-selection"/>
-                <span class="pl-[19.479vw] text-[#FFF] custom-selection">~Kevin Barnett</span>
+        <div class="quote-box mx-[32px] my-[32px]  absolute left-0 bottom-0">
+            <p class="quote text-[1.67vw] leading-[82%] tracking-tighter text-[#D9D9D9] font-sen custom-selection">
+                Simplicity will stand out, while complexity <br class="custom-selection"/>will get lost in the crowd.<br class="custom-selection"/>
+                <span class="speaker pl-[19.479vw] text-[#FFF] custom-selection">Kevin Barnett</span>
             </p>
         </div>
     </div>
@@ -161,18 +183,22 @@
 <!-- --------------- ABOUT SECTION ---------------- -->
 <section id="about" class="min-h-screen flex flex-col">
     <div class="h-[1px] bg-[#3872f0] mx-[15vw]"></div>
-    <div class="pr-[25.104vw] w-full flex flex-row justify-between relative">
-        <div class="w-[33.125vw] flex flex-col justify-end">
-            <div class="pl-[32px] pb-[3.333vw]">
-                <p class="font-sen font-medium leading-[100%] text-[1.667vw] text-white custom-selection">"Passionate about creating visually captivating and user-centric designs, I am a designer who thrives on turning ideas into stunning digital experiences. ut the way people interact with the world."</p>
-                <h1 class="font-sen font-bold leading-[100%] text-white text-[9.375vw] pt-[3.333vw] select-none" draggable="false">about</h1>
+    <div class="intro-mobile pr-[25.104vw] w-full flex flex-row justify-between relative ">
+        <div class="about-mobile w-[33.125vw] flex flex-col justify-end">
+            <div class="top-intro-mobile pl-[32px] pb-[3.333vw]">
+                <p class="about-text-mobile font-sen font-medium leading-[100%] text-[1.667vw] text-white custom-selection">"Passionate about creating visually captivating and user-centric designs, I am a designer who thrives on turning ideas into stunning digital experiences. ut the way people interact with the world."</p>
+                <h1 class="about-heading-mobile font-sen font-bold leading-[100%] text-white text-[9.375vw] pt-[3.333vw] select-none" draggable="false">about</h1>
             </div>
+
         </div>
-        <div class="w-[32.292vw] py-[3.33vw] flex flex-col justify-between">
-            <p class="font-medium text-[1.042vw] leading-[100%] text-[#CABBFF] text-right custom-selection">
-                "Passionate about creating visually captivating and user-centric designs, I am a designer who thrives on turning ideas into stunning digital experiences. With a keen eye for detail and a love for innovation, I specialize in crafting designs that not only look beautiful but also deliver seamless functionality. Whether it's UI/UX, graphic design, or creative branding, I aim to tell compelling stories through visuals that leave a lasting impression. Constantly exploring trends and pushing boundaries, I believe in the power of design to transform the way people interact with the world."
+
+        
+
+        <div class="photo-mobile w-[32.292vw] pt-[7.2vw] flex flex-col justify-end items-end gap-[1.5rem]">
+            <p class="photo-text-mobile font-medium text-[1.042vw] leading-[100%] text-[#CABBFF] text-right custom-selection w-[70%]">
+                "Passionate about creating visually captivating and user-centric designs, I am a designer who thrives on turning ideas into stunning dianding, I aim to tell compelling stories through visuals that leave a lasting impression. Constantly exploring trends and pushing boundaries, I believe in the power of design to transform the way people interact with the world."
             </p>
-            <div class="w-full h-[32.292vw] bg-gray-400">
+            <div class="photo-image-mobile w-[70%] h-[22.292vw] bg-gray-400">
                 
             </div>
         </div>
@@ -181,7 +207,7 @@
     <!-- About Continued -->
     <div class="w-[49.844vw] mx-auto my-[3.33vw] gap-0">
         <!-- Section 1 -->
-        <div class="h-[15.104vw] relative">
+        <div class="card h-[15.104vw] relative">
             <div id="card" class="w-[13.75vw] flex flex-col">
                 <div class="w-full h-[12vw] bg-green-600 cursor-pointer">
                     <!-- Insert image here -->
@@ -193,7 +219,7 @@
         </div>
         
         <!-- Section 2 -->
-        <div class="h-[18.49vw] relative">
+        <div class="card h-[18.49vw] relative">
             <div class="w-[15.625vw] flex flex-col absolute right-0">
                 <div class="w-full h-[16vw] bg-blue-400 cursor-pointer">
                     <!-- Insert image here -->
@@ -205,7 +231,7 @@
         </div>
 
         <!-- Section 3 -->
-        <div class="h-[15.313vw] relative">
+        <div class="card h-[15.313vw] relative">
             <div class="w-[19.792vw] flex flex-col">
                 <div class="w-full h-[13.5vw] bg-yellow-300 cursor-pointer">
                     <!-- Insert image here -->
@@ -217,7 +243,7 @@
         </div>
 
         <!-- Section 4 -->
-        <div class="h-[15.313vw] relative">
+        <div class="card h-[15.313vw] relative">
             <div class="w-[15.625vw] flex flex-col absolute right-0">
                 <div class="w-full h-[13.5vw] bg-red-700 cursor-pointer">
                     <!-- Insert image here -->
@@ -431,7 +457,7 @@ textarea:not(:placeholder-shown):invalid {
 
 /* Custom Scrollbar */
 :global(::-webkit-scrollbar) {
-    width: 5px; /* Scrollbar width */
+    width: 2px; /* Scrollbar width */
 }
 
 :global(::-webkit-scrollbar-track) {
@@ -447,6 +473,185 @@ textarea:not(:placeholder-shown):invalid {
 :global(::-webkit-scrollbar-thumb:hover) {
     background: #2527b9; /* Thumb hover color */
     border-radius: 10px
+}
+
+/* Navbar Responsive */
+@media (max-width: 35em){
+    /* Navbar responsiveness starts */
+    .primary-navigation {
+        position: fixed;
+        inset: 0 0 0 30%;
+        background: hsl(0 0% 0% / 0.0.75);
+        border-left: 1px solid;
+        flex-direction: column;
+        padding-top: min(30vh, 10rem);
+        transform: translateX(1000px);
+        transition: transform 500ms linear;
+    }
+
+    .primary-navigation.open {
+    transform: translateX(0); /* Slide in */
+    }
+
+    .mobile-nav-toggle{
+        position: fixed;
+        background-repeat: no-repeat;
+        border: 0;
+        width: 2rem;
+        aspect-ratio: 1;
+        top: 2rem;
+        right: 2rem;
+        z-index: 500;
+    }
+
+    .mobile-nav-toggle.hamburger {
+        background: url(/icon-hamburger.svg);
+        background-repeat: no-repeat;
+    }
+
+    .mobile-nav-toggle.close{
+        background: url(/icon-close.svg);
+        background-repeat: no-repeat;
+    }
+
+    .sr-only {
+        position: absolute;
+        width: 1px;
+        height: 1px;
+        padding: 0;
+        overflow: hidden;
+        clip: rect(0, 0, 0, 0);
+        white-space: nowrap;
+        border: 0;
+    }
+    /* Navbar responsiveness ends */
+
+    /* My name */
+    .intro{
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+    }
+
+    .my-name{
+        font-size: 36px;
+    }
+    .my-designnation{
+        font-size: 14px;
+    }
+
+    /* Quote */
+    .quote-box{
+        position: absolute;
+        left: 50%;
+        transform: translateX(-50%);
+        margin: 0;
+        text-align: center;
+        padding: 1rem;
+        min-width: 90%;
+    }
+
+    .quote{
+        font-size: 20px;
+    }
+
+    .speaker{
+        padding-left: 0;
+    }
+    /* --------- */
+
+    /* Date and Time */
+    .fixed-top{
+        position: fixed;
+        top: 0;
+        left: 0;
+        transform: translateX(-50%);
+        text-align: center;
+        /* background: #000; */
+        height: 14px;
+        font-size: 12px;
+        padding: .5rem 2rem;
+        margin: 1.5rem 5rem;
+    }
+
+    /* Assam India Logo start*/
+    .assam-svg{
+        display: none;
+    }
+
+
+    /* ---------ABOUT SECTION---------- */
+    .intro-mobile {
+        /* background: green; */
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        padding: 0;
+    }
+
+    .about-mobile{
+        width: 100%;
+        padding: 2rem;
+        display: flex;
+        flex-direction: column-reverse;
+    }
+
+    /*  */
+    .about-heading-mobile{
+        font-size: 64px;
+    }
+
+    .about-text-mobile{
+        font-size: 14px;
+        text-align: center;
+    }
+
+    .top-intro-mobile {
+        display: flex;
+        flex-direction: column-reverse;
+        gap: 1rem;
+        align-items: center;
+        padding: 0 2rem;
+    }
+
+    .photo-mobile{
+        width: 100%;
+        /* background: yellow; */
+        display: flex;
+        gap: 1rem;
+        justify-content: center;
+        align-items: center;
+        margin: 0;
+    }
+
+    .photo-text-mobile{
+        padding: 0rem 1rem;
+        padding-top: 2rem;
+        border-top: 1px solid white;
+        font-size: 15px;
+        text-align: center;
+        /* background: green; */
+    }
+
+    .photo-image-mobile{
+        width: min(40vh, 250px);
+        height: 250px;
+        margin-top: 0;
+    }
+
+    /* About Section Continued */
+    .card{
+        display: flex;
+        align-items: center;
+        justify-content: centre;
+        background: red;
+    }
+
+}
+
+@supports (backdrop-filter: blur(1rem)){
+    .primary-navigation: blur(1rem);
 }
 
 /* Gradient border */
